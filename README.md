0aadaa6a06743b9f4dd1330bb20136c04bdf0057
Nilton05 <jnbm1505@gmail.com>
2021-05-21 02:06:01 -0300

    Primeiro commit do desafio Cadastro Pessoal

---
+++ README.md
@@ -0,0 +1,13 @@
+	A página referente ao formulário de Cadastro Pessoal foi feita com a utilização do Visual Studio Code, como ferramenta de desenvolvimento. Como Browser, foi usado o Edge, da Microsoft.
+	As linguagens de implementação dos códigos foram o HTML5 e o JavaScript, prescindindo-se do CSS, para nós, deficientes visuais totais, no caso do presente desafio.
+
+	Ao ser carregada, a página apresenta, além dos campos, dois Selects, um referente ao Sexo, e o outro, à Unidade Federativa (UF), bem como dois botões, Cadastrar-se e Novo Cadastro. Relativamente aos botões, o primeiro aparece habilitado por padrão, e o segundo, desabilitado. Ao passar pelas validações de todos os campos obrigatórios, inverte-se o quadro, isto é, o Novo Cadastro passa a ficar habilitado, e o Cadastrar-se, desabilitado.
+
+	Além disso, é criada de forma dinâmica a tabela com os cabeçalhos somente dos campos obrigatórios, e nesse caso, na única linha, os seus respectivos dados, oriundos da digitação do usuário.
+
+	O botão Novo Cadastro, ao ser clicado, faz com que a página seja recarregada, zerando todos os campos, voltando a habilitar o botão Cadastre-se, desabilitando o Novo Cadastro, além de apagar a tabela criada anteriormente.
+
+Links de Referência
+
+	http://www.linhadecodigo.com.br/artigo/3617/eventos-em-javascript-tratando-eventos.aspx
+	https://cursos.alura.com.br/forum/topico-habitar-desabilitar-botao-65202
---
+++ js/index.js
@@ -0,0 +1,113 @@
+let botaoCadastrar = document.querySelector("#cadastrar");
+botaoCadastrar.addEventListener("click", function(event) {
+event.preventDefault();
+
+
+
+//pegando as informações digitadas nos campos
+let nome = document.querySelector("#nome").value;
+let cpf = document.querySelector("#cpf").value;
+let rg = document.querySelector("#rg").value;
+let sexo = document.querySelector("#sexo").value;
+
+let cep = document.querySelector("#cep").value;
+let endereco = document.querySelector("#endereco").value;
+let numero = document.querySelector("#numero").value;
+let bairro = document.querySelector("#bairro").value;
+let cidade = document.querySelector("#cidade").value;
+let uf = document.querySelector("#uf").value;
+let telFixo = document.querySelector("#tel-fixo").value;
+let telCelular = document.querySelector("#tel-cel").value;
+
+//validando os campos obrigatórios
+if (nome == '') {
+	document.querySelector("#nome").focus();
+}
+else if (cpf == '') {
+	document.querySelector("#cpf").focus();
+}
+else if (endereco == '') {
+	document.querySelector("#endereco").focus();
+}
+else if (numero == '') {
+	document.querySelector("#numero").focus();
+}
+else if (telCelular == '') {
+	document.querySelector("#tel-cel").focus();
+}
+
+else {
+
+
+//desabilitando o botão de cadastro e habilitando o de limpar o formulário
+document.querySelector("#cadastrar").disabled = true;
+document.querySelector("#cadastroNovo").disabled = false
+
+	//criando a tabela para exibir o resultado do cadastro
+let cadTable = document.createElement("table");
+
+//criando as ths para o cabeçalho da tabela
+let nomeTh = document.createElement("th");
+let cpfTh = document.createElement("th");
+let enderecoTh = document.createElement("th");
+let numeroTh = document.createElement("th");
+let celularTh = document.createElement("th");
+
+nomeTh.textContent = 'Nome';
+cpfTh.textContent = 'CPF';
+enderecoTh.textContent = 'Rua';
+numeroTh.textContent = 'Número';
+celularTh.textContent = 'Telefone Celular';
+
+//Adicionando as ths na tabela por meio da função appendChild
+cadTable.appendChild(nomeTh);
+cadTable.appendChild(cpfTh);
+cadTable.appendChild(enderecoTh);
+cadTable.appendChild(numeroTh);
+cadTable.appendChild(celularTh);
+
+//criando a tr (linha da tabela)
+let linhaTr = document.createElement("tr");
+
+//criando os tds (as colunas que irão conter os dados digitados)
+let nomeTd = document.createElement("td");
+let cpfTd = document.createElement("td");
+let enderecoTd = document.createElement("td");
+let numeroTd = document.createElement("td");
+let celularTd = document.createElement("td");
+
+//adicionando os valores nas colunas
+
+nomeTd.textContent = nome;
+cpfTd.textContent = cpf;
+enderecoTd.textContent = endereco;
+numeroTd.textContent = numero;
+celularTd.textContent = telCelular;
+
+//adicionando as colunas na linha, pela função appendChild
+linhaTr.appendChild(nomeTd);
+linhaTr.appendChild(cpfTd);
+linhaTr.appendChild(enderecoTd);
+linhaTr.appendChild(numeroTd);
+linhaTr.appendChild(celularTd);
+
+//adicionando a linha na tabela
+cadTable.appendChild(linhaTr);
+
+//trazendo a div para incluir nela a tabela
+let divResult = document.querySelector(".resultado");
+divResult.appendChild(cadTable);
+}
+});
+
+//trazendo o botão relativo ao novo cadastro
+let botaocadastroNovo = document.querySelector("#cadastroNovo");
+//adicionando um ouvinte ao botão
+botaocadastroNovo.addEventListener("click", function (event) {
+event.preventDefault();
+//recarregando a tela para zerar os campos e retirar a visualização da tabela
+window.location.reload();
+document.querySelector("#nome").focus();
+});
+
+
