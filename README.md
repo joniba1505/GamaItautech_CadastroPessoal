ecc5b2e2ea6998b86b5aa754a2d656027544fc05
Nilton05 <jnbm1505@gmail.com>
2021-07-29 21:27:10 -0300

    Alteração dos arquivos index.html e index.js, adicionando mais itens no cabeçalho da tabela e na sua respectiva linha.

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 	A página referente ao formulário de Cadastro Pessoal foi feita com a utilização do Visual Studio Code, como ferramenta de desenvolvimento. Como Browser, foi usado o Edge, da Microsoft.
-	As linguagens de implementação dos códigos foram o HTML5 e o JavaScript, prescindindo-se do CSS, para nós, deficientes visuais totais, no caso do presente desafio.
+	As linguagens de implementação dos códigos foram o HTML5 e o JavaScript. Pelo fato de ser facultativo o CSS a nós, deficientes visuais, resolvi não utilizá-lo no presente desafio, dando um enfoque maior na parte lógica do projeto.
 
-	Ao ser carregada, a página apresenta, além dos campos, dois Selects, um referente ao Sexo, e o outro, à Unidade Federativa (UF), bem como dois botões, Cadastrar-se e Novo Cadastro. Relativamente aos botões, o primeiro aparece habilitado por padrão, e o segundo, desabilitado. Ao passar pelas validações de todos os campos obrigatórios, inverte-se o quadro, isto é, o Novo Cadastro passa a ficar habilitado, e o Cadastrar-se, desabilitado.
+	Ao ser carregada, a página apresenta, além dos campos para serem preenchidos, dois Selects, um referente ao Sexo, e o outro, à Unidade Federativa (UF), bem como dois botões, Cadastrar-se e Novo Cadastro. Relativamente aos botões, o primeiro aparece habilitado por padrão, e o segundo, desabilitado. Ao passar pelas validações de todos os campos obrigatórios, quando o usuário clica no botão "Cadastrar-se", inverte-se o quadro, isto é, o Novo Cadastro passa a ficar habilitado, e o Cadastrar-se, desabilitado. Além disso, é criada de forma dinâmica a tabela com os cabeçalhos de todos os campos, e na única linha, os seus respectivos dados, oriundos da digitação e escolha por parte do usuário.
 
-	Além disso, é criada de forma dinâmica a tabela com os cabeçalhos somente dos campos obrigatórios, e nesse caso, na única linha, os seus respectivos dados, oriundos da digitação do usuário.
-
-	O botão Novo Cadastro, ao ser clicado, faz com que a página seja recarregada, zerando todos os campos, voltando a habilitar o botão Cadastre-se, desabilitando o Novo Cadastro, além de apagar a tabela criada anteriormente.
+	O botão "Novo Cadastro", ao ser clicado, faz com que a página seja recarregada, zerando todos os campos, voltando a habilitar o botão "Cadastre-se", desabilitando o botão "Novo Cadastro", além de apagar a tabela criada anteriormente.
 
 Links de Referência
 
--- js/index.js
+++ js/index.js
@@ -49,21 +49,43 @@ let cadTable = document.createElement("table");
 //criando as ths para o cabeçalho da tabela
 let nomeTh = document.createElement("th");
 let cpfTh = document.createElement("th");
+let rgTh = document.createElement("th");
+let sexoTh = document.createElement("th");
+let cepTh = document.createElement("th");
 let enderecoTh = document.createElement("th");
+
 let numeroTh = document.createElement("th");
+let bairroTh = document.createElement("th");
+let cidadeTh = document.createElement("th");
+let ufTh = document.createElement("th");
+let fixoTh = document.createElement("th");
 let celularTh = document.createElement("th");
 
 nomeTh.textContent = 'Nome';
 cpfTh.textContent = 'CPF';
+rgTh.textContent = 'RG';
+sexoTh.textContent = 'Sexo';
+cepTh.textContent = 'CEP';
 enderecoTh.textContent = 'Rua';
 numeroTh.textContent = 'Número';
+bairroTh.textContent = 'Bairro';
+cidadeTh.textContent = 'Cidade';
+ufTh.textContent = 'UF';
+fixoTh.textContent = 'Telefone Fixo';
 celularTh.textContent = 'Telefone Celular';
 
 //Adicionando as ths na tabela por meio da função appendChild
 cadTable.appendChild(nomeTh);
 cadTable.appendChild(cpfTh);
+cadTable.appendChild(rgTh);
+cadTable.appendChild(sexoTh);
+cadTable.appendChild(cepTh);
 cadTable.appendChild(enderecoTh);
 cadTable.appendChild(numeroTh);
+cadTable.appendChild(bairroTh);
+cadTable.appendChild(cidadeTh);
+cadTable.appendChild(ufTh);
+cadTable.appendChild(fixoTh);
 cadTable.appendChild(celularTh);
 
 //criando a tr (linha da tabela)
@@ -72,23 +94,44 @@ let linhaTr = document.createElement("tr");
 //criando os tds (as colunas que irão conter os dados digitados)
 let nomeTd = document.createElement("td");
 let cpfTd = document.createElement("td");
+let rgTd = document.createElement("td");
+let sexoTd = document.createElement("td");
+let cepTd = document.createElement("td");
 let enderecoTd = document.createElement("td");
 let numeroTd = document.createElement("td");
+let bairroTd = document.createElement("td");
+let cidadeTd = document.createElement("td");
+let ufTd = document.createElement("td");
+let fixoTd = document.createElement("td");
 let celularTd = document.createElement("td");
 
 //adicionando os valores nas colunas
 
 nomeTd.textContent = nome;
 cpfTd.textContent = cpf;
+rgTd.textContent = rg;
+sexoTd.textContent = sexo;
+cepTd.textContent = cep;
 enderecoTd.textContent = endereco;
 numeroTd.textContent = numero;
+bairroTd.textContent = bairro;
+cidadeTd.textContent = cidade;
+ufTd.textContent = uf;
+fixoTd.textContent = telFixo;
 celularTd.textContent = telCelular;
 
 //adicionando as colunas na linha, pela função appendChild
 linhaTr.appendChild(nomeTd);
 linhaTr.appendChild(cpfTd);
+linhaTr.appendChild(rgTd);
+linhaTr.appendChild(sexoTd);
+linhaTr.appendChild(cepTd);
 linhaTr.appendChild(enderecoTd);
 linhaTr.appendChild(numeroTd);
+linhaTr.appendChild(bairroTd);
+linhaTr.appendChild(cidadeTd);
+linhaTr.appendChild(ufTd);
+linhaTr.appendChild(fixoTd);
 linhaTr.appendChild(celularTd);
 
 //adicionando a linha na tabela
